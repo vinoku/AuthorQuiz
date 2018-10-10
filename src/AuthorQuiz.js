49da15595e947bcc592c096e3031575f1035c1e4
vinoku <vinoku78@gmail.com>
2018-10-10 21:18:10 +0200

    add test setup with basic tests

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 import './bootstrap.min.css';
 
 class AuthorQuiz extends Component {
   render() {
     return (
-      <div>Author Quizª</div>
+      <div>Author Quiz</div>
     );
   }
 }
--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AuthorQuiz from './AuthorQuiz';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<AuthorQuiz />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('When testing with Enzyme', () => {
+  it('renders a div', () => {
+    const wrapper = shallow(<AuthorQuiz/>);
+    expect(wrapper.find('div').length).toBe(1);
+  });
 });
